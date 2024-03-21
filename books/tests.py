fc6c79eea622299b9e59db4301a2620fb24c77b2
Behrooz Payoon <behrooz.payoon@gmail.com>
2024-03-22 01:47:27 +0330

    Add generic api list view and fix subtitle pronunciation in book model

--- books/tests.py
+++ books/tests.py
@@ -1,28 +1,22 @@
-from django.test import TestCase
 from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
 from .models import Book
 
-class BookTests(TestCase):
-  @classmethod
-  def setUpTestData(cls):
-    cls.book = Book.objects.create(
-      title="A good title",
-      subtitle="An excellent subtitle",
-      author="Tom Christie",
-      isbn="1234567890123",
-  )
-  
-  def test_book_content(self):
-    self.assertEqual(self.book.title, "A good title")
-    self.assertEqual(self.book.subtitle, "An excellent subtitle")
-    self.assertEqual(self.book.author, "Tom Christie")
-    self.assertEqual(self.book.isbn, "1234567890123")
 
-  def test_book_listview(self):
-    response = self.client.get(reverse("home"))
-    self.assertEqual(response.status_code, 200)
-    self.assertContains(response, "excellent subtitle")
+class APITests(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.book = Book.objects.create(
+            title="Book 1",
+            subtitle="Subtitle of book 1",
+            author="Behrooz",
+            isbn="12341234"
+        )
 
-    self.assertTemplateUsed(response, "books/book_list.html")
-
+    def test_api_list_view(self):
+        response = self.client.get(reverse("book_list"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Book.objects.count(), 1)
+        self.assertContains(response, self.book)
--- books/views.py
+++ books/views.py
@@ -1,8 +1,9 @@
-from django.views.generic import ListView
+from rest_framework import generics
 
 from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
-class BookListView(ListView):
-    model = Book
-    template_name = "book_list.html"
+
+class BookApiView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
